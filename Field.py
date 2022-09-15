f498cf93cf96b512a71e6946c26ed105ff6ba627
Agam Shushan <agam.shushan@gmail.com>
2022-09-15 17:43:32 +0300

    more functions

--- Field.py
+++ Field.py
@@ -10,34 +10,54 @@ def new_field():
     for i in range(Consts.FIELD_MATRIX_ROWS):
         if i >= Consts.FIELD_MATRIX_ROWS - Consts.FLAG_HEIGHT - 1:
             empty_field.append([
-                             Consts.NO_OBSTACLE] * (
-                                 Consts.FIELD_MATRIX_COLS - Consts.FLAG_WIDTH)
-                             + [Consts.FLAG] * Consts.FLAG_WIDTH)
+                                   Consts.NO_OBSTACLE] * (
+                                       Consts.FIELD_MATRIX_COLS - Consts.FLAG_WIDTH)
+                               + [Consts.FLAG] * Consts.FLAG_WIDTH)
         else:
             empty_field.append([Consts.NO_OBSTACLE] * Consts.FIELD_MATRIX_COLS)
 
     return empty_field
 
 
+def get_loc_tup():
+    row = random.randint(0, Consts.FIELD_MATRIX_ROWS)
+    if row < Consts.SOLDIER_HEIGHT:
+        col = random.randint(Consts.SOLDIER_WIDTH,
+                             Consts.FIELD_MATRIX_COLS - Consts.BUSH_WIDTH)
+    elif row >= Consts.FIELD_MATRIX_ROWS - Consts.FLAG_HEIGHT - 1:
+        col = random.randint(0,
+                             Consts.FIELD_MATRIX_COLS - Consts.FLAG_WIDTH
+                             - Consts.BUSH_WIDTH)
+    else:
+        col = random.randint(0,
+                             Consts.FIELD_MATRIX_COLS - Consts.BUSH_WIDTH)
+    loc_tup = (row, col)
+    return loc_tup
+
+
 def generate_bush_field():
     global field
     field = new_field()
+
     for i in range(Consts.NUM_OF_OBSTACLES):
-        row = random.randint(0, Consts.FIELD_MATRIX_ROWS)
-        if row < Consts.SOLDIER_HEIGHT:
-            col = random.randint(Consts.SOLDIER_WIDTH,
-                                 Consts.FIELD_MATRIX_COLS - Consts.BUSH_WIDTH)
-        elif row >= Consts.FIELD_MATRIX_ROWS - Consts.FLAG_HEIGHT - 1:
-            col = random.randint(0,
-                                 Consts.FIELD_MATRIX_COLS - Consts.FLAG_WIDTH
-                                 - Consts.BUSH_WIDTH)
-        else:
-            col = random.randint(0,
-                                 Consts.FIELD_MATRIX_COLS - Consts.BUSH_WIDTH)
-        for j in range(col, col + Consts.BUSH_WIDTH):
-            field[row][j] = Consts.BUSH
+        flag = True
+        while flag:
+            loc = get_loc_tup()
+            flag = if_free_for_bush(loc[0], loc[1])
+
+        for j in range(loc[1], loc[1] + Consts.BUSH_WIDTH):
+            for k in range(loc[0], loc[0] + Consts.BUSH_HEIGHT):
+                field[k][j] = Consts.BUSH
 
 
 def if_on_flag():
     location = Solider.get_loc()
     return field[location[0] + 1][location[1]] == Consts.FLAG
+
+
+def if_free_for_bush(row, col):
+    check_str = ''
+    for i in range(row, row + Consts.BUSH_HEIGHT):
+        for j in range(col, col + Consts.BUSH_WIDTH):
+            check_str += field[i, j]
+    return check_str == Consts.BUSH * Consts.BUSH_WIDTH * Consts.BUSH_HEIGHT
--- MineField.py
+++ MineField.py
@@ -10,19 +10,13 @@ def generate_mine_field():
     global mine_field
     mine_field = Field.new_field()
     for i in range(Consts.NUM_OF_OBSTACLES):
-        row = random.randint(0, Consts.FIELD_MATRIX_ROWS)
-        if row < Consts.SOLDIER_HEIGHT:
-            col = random.randint(Consts.SOLDIER_WIDTH,
-                                 Consts.FIELD_MATRIX_COLS - Consts.MINE_WIDTH)
-        elif row >= Consts.FIELD_MATRIX_ROWS - Consts.FLAG_HEIGHT - 1:
-            col = random.randint(0,
-                                 Consts.FIELD_MATRIX_COLS - Consts.FLAG_WIDTH
-                                 - Consts.MINE_WIDTH)
-        else:
-            col = random.randint(0,
-                                 Consts.FIELD_MATRIX_COLS - Consts.MINE_WIDTH)
-        for j in range(col, col + Consts.MINE_WIDTH):
-            mine_field[row][j] = Consts.MINE
+        flag = True
+        while flag:
+            loc = Field.get_loc_tup()
+            flag = if_free_for_mine(loc[0], loc[1])
+
+        for j in range(loc[1], loc[1] + Consts.MINE_WIDTH):
+            mine_field[loc[0]][j] = Consts.MINE
 
 
 def if_on_mine():
@@ -31,3 +25,10 @@ def if_on_mine():
     left_leg = (location[0] - 3, location[1] + 1)
     return (mine_field[right_leg[0]][right_leg[1]] == Consts.MINE or
             mine_field[left_leg[0]][left_leg[1]] == Consts.MINE)
+
+
+def if_free_for_mine(row, col):
+    check_str = ''
+    for i in range(row, row + Consts.MINE_WIDTH):
+        check_str += mine_field[i, j]
+    return check_str == Consts.BUSH * Consts.MINE_WIDTH
