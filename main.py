3cef0add02327f423b129df28bce673f1ac6b3e5
RonyGildor <ronygildor@gmail.com>
2022-09-18 10:00:00 +0300

    main

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import pygame
+import Consts
+import MineField
+import Solider
+import Screen
+import Field
+
+
+
 def main():
-    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
-    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     is_first = True
     clock = pygame.time.Clock()
     is_run = True
     while is_run:
-        clock.tick(FPS)
+        clock.tick(Consts.FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_run = False
@@ -15,13 +23,11 @@ def main():
         if is_first:
             keys_pressed_last_turn = keys_pressed
             is_first = False
-        # movement(keys_pressed, keys_pressed_last_turn)
-        red_movement(red, keys_pressed)
+        Screen.movement(keys_pressed, keys_pressed_last_turn)
         keys_pressed_last_turn = keys_pressed
 
 
 
-        draw_window(red, yellow)
 
     pygame.quit()
 
