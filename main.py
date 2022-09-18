6709c04fef7b60432e851ef0e751c8a164df91fd
RonyGildor <ronygildor@gmail.com>
2022-09-18 11:59:41 +0300

    main update

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import Solider
 import Screen
 import Field
 
-# def is_win_or_lose():
-#     check = True
-#     if MineField.if_on_mine():
-#         Screen.draw_lose_message()
-#         check = False
-#     elif Field.if_on_flag():
-#         Screen.draw_win_message()
-#         check = False
-#     return check
+def is_win_or_lose():
+    check = True
+    if MineField.if_on_portal():
+        Screen.draw_lose_message()
+        check = False
+    elif Field.if_on_ship():
+        Screen.draw_win_message()
+        check = False
+    return check
 
 
 def main():
-    MineField.generate_mine_field()
-    Field.generate_bush_field()
+    MineField.generate_portal_field()
+    Field.generate_head_field()
     is_first = True
     clock = pygame.time.Clock()
     is_run = True
@@ -37,7 +37,7 @@ def main():
         Solider.move_soldier(Screen.movement(keys_pressed, keys_pressed_last_turn))
         keys_pressed_last_turn = keys_pressed
 
-        #is_run = is_win_or_lose()
+        is_run = is_win_or_lose()
 
 
 
