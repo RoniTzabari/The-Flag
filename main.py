7ee14afcd8ce1426b308cdb704b20ac9e8c9c467
RonyGildor <ronygildor@gmail.com>
2022-09-18 10:18:17 +0300

    win and lose messages in main

--- main.py
+++ main.py
@@ -6,9 +6,24 @@ import Screen
 import Field
 
 
+def draw_lose_message():
+    draw_message(Consts.LOSE_MESSAGE, Consts.LOSE_FONT_SIZE,
+                 Consts.LOSE_COLOR, Consts.LOSE_LOCATION)
+
+
+def draw_win_message():
+    draw_message(Consts.WIN_MESSAGE, Consts.WIN_FONT_SIZE,
+                 Consts.WIN_COLOR, Consts.WIN_LOCATION)
+
+
+def draw_message(message, font_size, color, location):
+    font = pygame.font.SysFont(Consts.FONT_NAME, font_size)
+    text_img = font.render(message, True, color)
+    Consts.WINDOW.blit(text_img, location)
 
 def main():
-
+    MineField.generate_mine_field()
+    Field.generate_bush_field()
     is_first = True
     clock = pygame.time.Clock()
     is_run = True
@@ -23,8 +38,17 @@ def main():
         if is_first:
             keys_pressed_last_turn = keys_pressed
             is_first = False
-        Screen.movement(keys_pressed, keys_pressed_last_turn)
+        Solider.move_soldier(Screen.movement(keys_pressed, keys_pressed_last_turn))
         keys_pressed_last_turn = keys_pressed
+
+        if MineField.if_on_mine():
+            is_run = False
+        elif Field.if_on_flag():
+            is_run
+
+        Screen.update_starter_screen(Field.get_bush_field())
+
+
 
 
 
