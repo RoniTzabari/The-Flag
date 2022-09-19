385875855742a102f968d6f77a393a8062fb37a6
Agam Shushan <agam.shushan@gmail.com>
2022-09-19 11:40:17 +0300

    read data

--- Database.py
+++ Database.py
@@ -5,15 +5,11 @@ import Solider
 
 
 # Read file
-def get_game_state(index):
+def change_game_state(index):
     data = pd.read_csv("GameData.csv", header=0)
-    print(data)
     Field.set_head_field(data.Heads[index])
     MineField.set_portal_field(data.Portals[index])
     Solider.set_loc(data.Player[index])
-    # game_state = {"field": data.Heads[key_pressed - 1],
-    #               "portal_field": data.Portals[key_pressed - 1],
-    #               "soldier_loc": data.Player[key_pressed - 1]}
 
 
 get_game_state(0)
