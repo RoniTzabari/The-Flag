c38f6514599ec1ea4d57dc36c9beb730d9f9b19e
RonyGildor <ronygildor@gmail.com>
2022-09-19 11:27:25 +0300

    check which numbers is pressed

--- main.py
+++ main.py
@@ -16,42 +16,6 @@ def is_win_or_lose():
         check = False
     return check
 
-def which_number_is_pressed(keys_pressed):
-    pressed_num = 0
-    if keys_pressed[pygame.K_1]:
-        pressed_num = pygame.K_1
-    elif keys_pressed[pygame.K_2]:
-        pressed_num = pygame.K_2
-    elif keys_pressed[pygame.K_3]:
-        pressed_num = pygame.K_3
-    elif keys_pressed[pygame.K_4]:
-        pressed_num = pygame.K_4
-    elif keys_pressed[pygame.K_5]:
-        pressed_num = pygame.K_5
-    elif keys_pressed[pygame.K_6]:
-        pressed_num = pygame.K_6
-    elif keys_pressed[pygame.K_7]:
-        pressed_num = pygame.K_7
-    elif keys_pressed[pygame.K_8]:
-        pressed_num = pygame.K_8
-    elif keys_pressed[pygame.K_9]:
-        pressed_num = pygame.K_9
-    return pressed_num
-
-def check_if_save_or_open(event):
-    if event.type == pygame.KEYDOWN:
-        # detect key 'a'
-        if event.key == pygame.K_1 or event.key == pygame.K_2 or event.key == pygame.K_3 or event.key == pygame.K_4 or event.key == pygame.K_5 or event.key == pygame.K_6 or event.key == pygame.K_7 or event.key == pygame.K_8 or event.key == pygame.K_9:
-            # key 'a'
-            start_time = time.time()
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_1 or event.key == pygame.K_2 or event.key == pygame.K_3 or event.key == pygame.K_4 or event.key == pygame.K_5 or event.key == pygame.K_6 or event.key == pygame.K_7 or event.key == pygame.K_8 or event.key == pygame.K_9:
-            press_duration = time.time() - start_time
-            if press_duration <= 1.0:
-                print("save game")
-            else:
-                print("open game")
-
 
 def main():
     MineField.generate_portal_field()
